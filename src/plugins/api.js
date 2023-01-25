8d2a87ac1bbc4d4c8f8615156fa46d9c33d47349
dimitarkolev <dk@almero.bg>
2023-01-25 14:55:23 +0000

    Api fix

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -17,53 +17,57 @@ async function syncEntities(apiName) {
     
     let synced = 0
     let removed = 0
-    const recent = new Set()
-    const entities = await readMany(mikser)
-    for (let meta of entities) {
-        if (collection && type && meta.id) {
-            const name = path.join(collection, meta.name || meta.id.toString())
-            const id = path.join('/api', collection, meta.id.toString())
-            if (recent.has(id)) {
-                logger.error(meta, 'Duplicate entity found: %s', id)
-                continue
-            }
-            recent.add(id)
-            const entity = normalize({
-                id,
-                uri: `${uri}/${meta.id}`,
-                name,
-                collection,
-                type,
-                format,
-                meta
-            })
-            entity.checksum = await hasha(JSON.stringify(entity.meta), { algorithm: 'md5' })
-            const current = await findEntity({ id })
-            if (current) {
-                if (entity.checksum != current.checksum) {
-                    await updateEntity(entity)
+    try {
+        const recent = new Set()
+        const entities = await readMany(mikser)
+        for (let meta of entities) {
+            if (collection && type && meta.id) {
+                const name = path.join(collection, meta.name || meta.id.toString())
+                const id = path.join('/api', collection, meta.id.toString())
+                if (recent.has(id)) {
+                    logger.error(meta, 'Duplicate entity found: %s', id)
+                    continue
+                }
+                recent.add(id)
+                const entity = normalize({
+                    id,
+                    uri: `${uri}/${meta.id}`,
+                    name,
+                    collection,
+                    type,
+                    format,
+                    meta
+                })
+                entity.checksum = await hasha(JSON.stringify(entity.meta), { algorithm: 'md5' })
+                const current = await findEntity({ id })
+                if (current) {
+                    if (entity.checksum != current.checksum) {
+                        await updateEntity(entity)
+                        synced++
+                    }
+                } else {
+                    await createEntity(entity)
                     synced++
                 }
-            } else {
-                await createEntity(entity)
-                synced++
             }
         }
-    }
-
-    const entitiesToRemove = await findEntities(entity => 
-        entity.type == type && 
-        entity.format == format && 
-        entity.collection == collection && 
-        entity.time < syncTime && 
-        !recent.has(entity.id)
-    )
-    for (let entity of entitiesToRemove) {
-        deleteEntity(entity)
-        removed++
-    }
-    if (synced || removed) {
-        logger.info('Syncing api: [%s] synced: %d, removed: %d', collection, synced, removed)
+    
+        const entitiesToRemove = await findEntities(entity => 
+            entity.type == type && 
+            entity.format == format && 
+            entity.collection == collection && 
+            entity.time < syncTime && 
+            !recent.has(entity.id)
+        )
+        for (let entity of entitiesToRemove) {
+            deleteEntity(entity)
+            removed++
+        }
+        if (synced || removed) {
+            logger.info('Syncing api: [%s] synced: %d, removed: %d', collection, synced, removed)
+        }
+    } catch (err) {
+        logger.error('Api sync entities error: %s', err.message)
     }
     return synced > 0 || removed > 0
 }
@@ -76,36 +80,40 @@ async function syncEntity(apiName, apiId) {
         readOne, 
         uri = '' 
     } = mikser.config.api[apiName]
-    
-    const id = path.join('/api', collection, apiId.toString())
-    const current = await findEntity({ id })
-    const meta = await readOne(apiId, mikser)
-    if (meta?.id) {
-        const name = path.join(collection, meta.name || meta.id.toString())
-        const entity = normalize({
-            id,
-            uri: `${uri}/${meta.id}`,
-            name,
-            collection,
-            type,
-            format,
-            meta
-        })
-        entity.checksum = await hasha(JSON.stringify(entity.meta), { algorithm: 'md5' })
-        if (current) {
-            if (entity.checksum != current.checksum) {
-                logger.info('Api update: %s', id)
-                await updateEntity(entity)
+
+    try {
+        const id = path.join('/api', collection, apiId.toString())
+        const current = await findEntity({ id })
+        const meta = await readOne(apiId, mikser)
+        if (meta?.id) {
+            const name = path.join(collection, meta.name || meta.id.toString())
+            const entity = normalize({
+                id,
+                uri: `${uri}/${meta.id}`,
+                name,
+                collection,
+                type,
+                format,
+                meta
+            })
+            entity.checksum = await hasha(JSON.stringify(entity.meta), { algorithm: 'md5' })
+            if (current) {
+                if (entity.checksum != current.checksum) {
+                    logger.info('Api update: %s', id)
+                    await updateEntity(entity)
+                }
+            } else {
+                logger.info('Api create: %s', id)
+                await createEntity(entity)
             }
         } else {
-            logger.info('Api create: %s', id)
-            await createEntity(entity)
+            if (current) {
+                logger.info('Api delete: %s', id)
+                await deleteEntity(entity)
+            }
         }
-    } else {
-        if (current) {
-            logger.info('Api delete: %s', id)
-            await deleteEntity(entity)
-        }
+    } catch (err) {
+        logger.error('Api sync entity error: %s', err.message)
     }
 }
 
