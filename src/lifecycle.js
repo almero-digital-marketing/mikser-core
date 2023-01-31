8085de621fc638093dea9bbb663c59967d103df3
dimitarkolev <dk@almero.bg>
2023-01-31 07:36:51 +0000

    Bump

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -24,20 +24,56 @@ export async function onImported(callback) {
     mikser.hooks.imported.push(callback)
 }
 
-export async function onProcess(callback) {
-    mikser.hooks.process.push(callback)
+export async function onProcess(callback, once) {
+    if (!once) mikser.hooks.process.push(callback)
+    else {
+        let called = false
+        mikser.hooks.process.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onProcessed(callback) {
-    mikser.hooks.processed.push(callback)
+export async function onProcessed(callback, once) {
+    if (!once) mikser.hooks.processed.push(callback)
+    else {
+        let called = false
+        mikser.hooks.processed.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onPersist(callback) {
-    mikser.hooks.persist.push(callback)
+export async function onPersist(callback, once) {
+    if (!once) mikser.hooks.persist.push(callback)
+    else {
+        let called = false
+        mikser.hooks.persist.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onPersisted(callback) {
-    mikser.hooks.persisted.push(callback)
+export async function onPersisted(callback, once) {
+    if (!once) mikser.hooks.persisted.push(callback)
+    else {
+        let called = false
+        mikser.hooks.persisted.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
 export async function onCancel(callback) {
@@ -48,24 +84,69 @@ export async function onCancelled(callback) {
     mikser.hooks.cancelled.push(callback)
 }
 
-export async function onBeforeRender(callback) {
-    mikser.hooks.beforeRender.push(callback)
+export async function onBeforeRender(callback, once) {
+    if (!once) mikser.hooks.beforeRender.push(callback)
+    else {
+        let called = false
+        mikser.hooks.beforeRender.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onRender(callback) {
-    mikser.hooks.render.push(callback)
+export async function onRender(callback, once) {
+    if (!once) mikser.hooks.render.push(callback)
+    else {
+        let called = false
+        mikser.hooks.render.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onAfterRender(callback) {
-    mikser.hooks.afterRender.push(callback)
+export async function onAfterRender(callback, once) {
+    if (!once) mikser.hooks.afterRender.push(callback)
+    else {
+        let called = false
+        mikser.hooks.afterRender.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onFinalize(callback) {
-    mikser.hooks.finalize.push(callback)
+export async function onFinalize(callback, once) {
+    if (!once) mikser.hooks.finalize.push(callback)
+    else {
+        let called = false
+        mikser.hooks.finalize.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
-export async function onFinalized(callback) {
-    mikser.hooks.finalized.push(callback)
+export async function onFinalized(callback, once) {
+    if (!once) mikser.hooks.finalized.push(callback)
+    else {
+        let called = false
+        mikser.hooks.finalized.push((signal) => {
+            if (!called) {
+                called = true
+                callback(signal)
+            } 
+        })
+    }
 }
 
 export async function onSync(name, callback) {
