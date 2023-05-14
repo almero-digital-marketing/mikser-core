7c4e50ba955b1c2ba2f53e8eca78a353efabe02a
dimitarkolev <dk@almero.bg>
2023-05-14 15:52:25 +0000

    Fix

--- src/plugins/render/resource.js
+++ src/plugins/render/resource.js
@@ -1,11 +1,11 @@
 export function load({ runtime, state }) {
     runtime.resource = (url) => {
-        const { resourceMap } = state.resources
-        for (let library in resourceMap) {
+        const { resourceLib } = state.resources
+        for (let library in resourceLib) {
             if (url.match(library)) {
                 const { origin } = new URL(url)
-                const name = url.replace(origin, `${resourceMap[library]}/`)
-                const link = url.replace(origin, `/${state.resources.resourcesFolder}/${resourceMap[library]}/`)
+                const name = url.replace(origin, `${resourceLib[library]}/`)
+                const link = url.replace(origin, `/${state.resources.resourcesFolder}/${resourceLib[library]}/`)
                 return { link, name }
             }
         }
